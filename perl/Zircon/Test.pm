7b92966c0eb21e5f71a9ae8bf73e5085b14c59a7
Jeremy Henty <jh13@sanger.ac.uk>
2012-05-16 16:29:08 +0100

    Test the "Goodbye" command.

--- perl/Zircon/Test.pm
+++ perl/Zircon/Test.pm
@@ -137,6 +137,86 @@ The child window closes.
 
 =back
 
+=item * Run "zircon_remotecontrol_test".
+
+=over
+
+=item * Click the "Goodbye" button in the child window.
+
+=over
+
+=item * Check
+
+The child window closes.
+
+=item * Check
+
+The parent window announces that as a server it has received a goodbye.
+
+=back
+
+=item * Click the "Ping" button in the parent window.
+
+=over
+
+=item * Check
+
+The parent window announces an error as the connection is closed.
+
+=back
+
+=item * Click the "Exit" button in the parent window.
+
+=over
+
+=item * Check
+
+The parent window closes.
+
+=back
+
+=back
+
+=item * Run "zircon_remotecontrol_test".
+
+=over
+
+=item * Click the "Goodbye" button in the parent window.
+
+=over
+
+=item * Check
+
+The parent window closes.
+
+=item * Check
+
+The child window announces that as a server it has received a goodbye.
+
+=back
+
+=item * Click the "Ping" button in the child window.
+
+=over
+
+=item * Check
+
+The child window announces an error as the connection is closed.
+
+=back
+
+=item * Click the "Exit" button in the child window.
+
+=over
+
+=item * Check
+
+The child window closes.
+
+=back
+
+=back
+
 =back
 
 =back
