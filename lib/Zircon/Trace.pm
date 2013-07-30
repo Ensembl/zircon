ee493769b2ba66535c9222ba554fcc16d0990dd0
Matthew Astley <mca@sanger.ac.uk>
2013-07-30 10:55:50 +0100

    Zircon::Trace - move colour+stamp out of bin/zircon_pipe
    all Zircon traces & unrelated warns can be coloured,
     timestamped or silenced.  e.g.
    
      ZIRCON_TRACE=colour,stamp  \
      ZIRCON_CONNECTION_TRACE=1 ZIRCON_SELECTION_TRACE=1 \
      prove -lv t/protocol.t

--- lib/Zircon/Trace.pm
+++ lib/Zircon/Trace.pm
@@ -4,21 +4,6 @@ package Zircon::Trace;
 use strict;
 use warnings;
 
-sub trace_env_update {
-    my ($called) = @_;
-    return unless Bio::Otter::Debug->can('debug');
-
-    if (Bio::Otter::Debug->debug('Zircon')) {
-        $ENV{$_}=1 foreach
-          (qw( ZIRCON_ZMAP_TRACE
-               ZIRCON_CONNECTION_TRACE ZIRCON_PROTOCOL_TRACE
-               ZIRCON_CONTEXT_TRACE    ZIRCON_SELECTION_TRACE ));
-        # we don't admit being able to turn it off again,
-        # because maybe it was set via %ENV from outside
-    }
-
-    return;
-}
 
 sub zircon_trace {
     my ($self, $format, @args) = @_;
@@ -40,6 +25,68 @@ sub zircon_trace {
 }
 
 sub zircon_trace_prefix {
+    return;
+}
+
+
+our %TRACE; # for e.g. ZIRCON_TRACE=sortable,colour,stamp
+sub trace_env_update {
+    my ($called) = @_;
+    return if keys %TRACE; # run no more than once per Perl
+
+    # Maybe set extra tracing - for otterlace
+    if (Bio::Otter::Debug->can('debug') &&
+        Bio::Otter::Debug->debug('Zircon')) {
+        $ENV{$_}=1 foreach
+          (qw( ZIRCON_ZMAP_TRACE
+               ZIRCON_CONNECTION_TRACE ZIRCON_PROTOCOL_TRACE
+               ZIRCON_CONTEXT_TRACE    ZIRCON_SELECTION_TRACE ));
+        # we don't admit being able to turn it off again,
+        # because maybe it was set via %ENV from outside
+    }
+
+    # Maybe change output style
+    if ($ENV{ZIRCON_TRACE}) {
+        foreach my $kvp (split ',', $ENV{ZIRCON_TRACE}) {
+            my ($k, $v) = $kvp =~ m{^(.*?)(?:=(.*))?$};
+            $TRACE{$k} = defined $v ? $v : 1;
+        }
+        $called->_warn_hook;
+    }
+    $TRACE{done_env} = 1;
+
+    return;
+}
+
+sub _warn_hook {
+    $ENV{ZIRCON_TRACE} .= ',child';
+    $TRACE{t0} = 1000*int(time() /1000); # short timestamps, locally synchronised
+
+    require Time::HiRes;
+    $SIG{__WARN__} = \&__warn_handler;
+
+    return;
+}
+
+# Can handles the simplistic case of one connection, colouring all
+# warn()s by (are we the child process?)
+sub __warn_handler {
+    my ($msg) = @_;
+    chomp $msg;
+
+    return # Ignore
+      if $TRACE{silent};
+
+    $msg =~ s{\n}{\\N}g # Protect the ability to sort(1) output
+      if $TRACE{sortable};
+
+    my $clr = $TRACE{child} ? 36 : 33;
+    $msg = qq{\x1B[${clr}m$msg\x1B[00m} if $TRACE{colour};
+
+    $msg = sprintf('%11.6f: %s', Time::HiRes::tv_interval([ $TRACE{t0}, 0 ]),
+                   $msg) if $TRACE{stamp};
+
+    print STDERR "$msg\n";
     return;
 }
 
